83e9155fca006c0eae3df96f649abeadee6fc10c
Nikita Poskryakov <83550057+SharapovTact@users.noreply.github.com>
2026-09-16 10:56:09 +0300

    Сделал задание 2, с арифметикой

---
+++ lab3/abc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1;
+    int num2;
+    cout << "Введите первое число: ";
+    cin >> num1;
+    cout << "Введите второе число: ";
+    cin >> num2;
+    cout << "Сумма чисел: " << num1 + num2 << endl;
+}
